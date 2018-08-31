f25c752947b867429de7b341f7d95859dbc6a3ce
lp-code <lp-code@users.noreply.github.com>
2018-08-31 12:30:03 +0200

    Initial commit, project datastructure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyse the trip data from the bicycle sharing scheme in Oslo.',
+    author='Lars Pesch',
+    license='',
+)
