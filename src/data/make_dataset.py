c93476a48fa95cc153c3242c8b0f78c087577191
Lars Pesch <lars.pesch@gmail.com>
2018-09-03 10:20:25 +0200

    Adapt make_dataset.py to bysykkel data.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-import click
+#import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+#from dotenv import find_dotenv, load_dotenv
 
+import os
+import bysykkel_data
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+#@click.command()
+#@click.argument('input_filepath', type=click.Path(exists=True))
+#@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -15,6 +17,15 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    max_trip_duration = 7200
+    df = bysykkel_data.read_trip_data(input_filepath)
+    df = bysykkel_data.augment_trip_data(df)
+    df = bysykkel_data.remove_invalid_trips(df, max_trip_duration)
+    print(df.head())
+    df.reset_index(inplace=True)
+    print(df.head())
+    df.to_feather(os.sep.join([output_filepath, 'trips.feather']))
+    
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -25,6 +36,13 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    #load_dotenv(find_dotenv())
 
-    main()
+    raw_data_path = os.sep.join(['..', '..', 'data', 'raw'])
+    interim_data_path = os.sep.join(['..', '..', 'data', 'interim'])
+    if not os.path.exists(raw_data_path):
+        raw_data_path = raw_data_path[6:]
+        interim_data_path = interim_data_path[6:]
+
+    print(raw_data_path, interim_data_path)
+    main(raw_data_path, interim_data_path)
