15c2cabeb92cb8545bfc51e9612acc28b83a0672
lp-code <lp-code@users.noreply.github.com>
2018-09-03 10:20:25 +0200

    Adapt make_dataset.py to bysykkel data.

---
+++ src/data/bysykkel_data.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import pandas as pd
+
+def read_trip_data(data_dir):
+    trip_files = os.listdir(data_dir)
+    trip_files.remove('.gitkeep')
+    logger = logging.getLogger(__name__)
+    logger.info('Raw data files: %s' % (str(trip_files)))
+    df_list = []
+    for trip_file in [trip_files[0]]:
+        logger.info('Reading: %s' % (trip_file))
+        df_list.append(pd.read_csv(data_dir + os.sep + trip_file, compression='zip',
+                                   parse_dates=[1, 3], infer_datetime_format=True))
+
+    logger.info('Done reading data from raw files.')
+    df = pd.concat(df_list, ignore_index=True)
+    return df
+
+
+def augment_trip_data(df):
+    df['Duration'] = df['End time'] - df['Start time']
+    df['DurationSeconds'] = df['Duration'].dt.total_seconds()
+    
+    return df
+
+
+def remove_invalid_trips(df, max_trip_duration):
+    df = df[(df.DurationSeconds >= 0)&(df.DurationSeconds < max_trip_duration)]
+
+    return df
+
+
+if __name__ == '__main__':
+    print('Module only.')
