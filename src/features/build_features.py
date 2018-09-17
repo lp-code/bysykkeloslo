61a2aedc1837956fb8275eb1226d035511eb1800
Hagenbrock99 <reinhard.hagenbrock@capgemini.com>
2018-09-17 13:50:32 +0200

    RH: Introduce calculation of solar elevation angle
    Take care: 5-min data is for now commented out. The calculation provides solar elevation angle in degrees above horizon and in categories of 6 degrees width

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,6 +2,10 @@
 
 import logging
 import numpy as np
+import astropy.coordinates as coord
+from astropy.time import Time
+import time, datetime
+from datetime import datetime, timedelta
 import os
 import pandas as pd
 from pathlib import Path
@@ -46,6 +50,57 @@ def met_transform_wind_direction(df, delete_original=True):
         df.drop(columns='wind_direction', inplace=True)
     return df
 
+def calculate_elevation(time,loc):
+    sun_time = Time(time)
+    if ( (time.hour == 0) and (time.minute == 0) ):
+        print(f'Calculate solar elevation angle for: {time}')
+    zen_ang = coord.get_sun(sun_time).transform_to(coord.AltAz(obstime=sun_time, location=loc)).alt.degree
+    return zen_ang
+
+def elevation_to_category(solar_elevation,degrees_per_cat,nr_of_categories):
+    upper = 0.
+    for icategory in range(nr_of_categories):
+        if (solar_elevation < upper):
+            return icategory
+        upper += degrees_per_cat
+    return nr_of_categories
+
+def insert_timesteps(df,timestepinterval):
+    dfarray = df.values
+    dflist = []
+    for dfline in dfarray:
+        totalinterval=0
+        while (totalinterval < 60):
+            dflist.append(dfline)
+            dfline = dfline + timedelta(minutes = timestepinterval)
+            totalinterval += timestepinterval
+    dfarray = np.asarray(dflist)
+    newdf = pd.DataFrame({'index':dfarray})
+    return newdf
+
+def elev_calculate_elevation(df, df_times,loc):
+    '''Calculate solar elevation angle in degrees above horizon
+    based on time and location'''
+
+    nr_of_categories = 15
+    degrees_per_cat = 90. / nr_of_categories
+
+    df_elev = []
+    df_elev_cat = []
+    for time in df_times:
+
+        elev = calculate_elevation(time,loc)
+        elev_cat = elevation_to_category(elev,degrees_per_cat,nr_of_categories)
+        df_elev.append(elev)
+        df_elev_cat.append(elev_cat)
+
+    # df_elev = df_times.apply(calculate_elevation,args=(loc))
+    df['solar_elevation_angle'] = pd.DataFrame(df_elev)
+    df['solar_elevation_angle'].astype('category')
+    df['solar_elevation_cat'] = pd.DataFrame(df_elev_cat)
+    df['solar_elevation_cat'].astype('category')
+
+    return df
 
 class WMO4677_translator(object):
     '''
