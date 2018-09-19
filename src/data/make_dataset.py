1e5ee38ddb76cb816757c96a9e23461275d79779
Lars Pesch <lars.pesch@gmail.com>
2018-09-19 09:41:06 +0200

    Investigate missing weather data. Conclusion: mostly night, have to drop when joined with demand.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,31 +7,30 @@ import os
 import bysykkel_data
 import met_data
 
-def main(input_filepath, output_filepath):
+def main(input_filepath, output_filepath,
+         get_bike_data=False, get_met_data=False):
     """ Runs data processing scripts to read raw data from (../raw), or get it
         from the internet, and save it in suitable format into data/interim.
         That data is read again by the featuer engineering script.
     """
     logger = logging.getLogger(__name__)
-    logger.info('Making the interim trip data set from raw data')
-
-    max_trip_duration = 14400
-    df = bysykkel_data.read_trip_data(input_filepath)
-    df = bysykkel_data.augment_trip_data(df)
-    df = bysykkel_data.remove_invalid_trips(df, max_trip_duration)
-    df.sort_values(by='Start time', inplace=True)
-
-    df.reset_index(inplace=True) # without this feather gives an error
-    logger.info('Write dataframe to feather format.')
-    df.to_feather(os.sep.join([output_filepath, 'trips.feather']))
-    #logger.info('Write dataframe to csv (zipped).')
-    #df.to_csv(os.sep.join([output_filepath, 'trips.csv']),
-    #          compression='zip')
     
-    if False:
-        logger.info('Making the interim weather data set from raw data')
+    if get_bike_data:
+        logger.info('Making the interim trip data set from raw data.')
+        max_trip_duration = 14400
+        df = bysykkel_data.read_trip_data(input_filepath)
+        df = bysykkel_data.augment_trip_data(df)
+        df = bysykkel_data.remove_invalid_trips(df, max_trip_duration)
+        df.sort_values(by='Start time', inplace=True)
+    
+        df.reset_index(inplace=True) # without this feather gives an error
+        logger.info('Write dataframe to feather format.')
+        df.to_feather(os.sep.join([output_filepath, 'trips.feather']))
+    
+    if get_met_data:
+        logger.info('Get met data from frost & write interim file.')
         met_data.get_met_data(output_filepath)
-    
+        logger.info('Done writing met data interim file.')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -43,5 +42,5 @@ if __name__ == '__main__':
     raw_data_path = os.sep.join([str(project_dir), 'data', 'raw'])
     interim_data_path = os.sep.join([str(project_dir), 'data', 'interim'])
 
-    print(raw_data_path, interim_data_path)
-    main(raw_data_path, interim_data_path)
+    main(raw_data_path, interim_data_path,
+         get_bike_data=False, get_met_data=True)
