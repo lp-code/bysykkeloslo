aedc1bfec1e84935ea9aa48e93a8d5dcf6b45f9a
Lars Pesch <lars.pesch@gmail.com>
2018-09-28 14:39:03 +0200

    Carry out grid search with cross-validation+put results into predict_model.

--- src/models/grid_search.py
+++ src/models/grid_search.py
@@ -5,16 +5,22 @@ import pandas as pd
 import pprint
 import re
 import sys
-import tqdm
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # modelling stuff
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
 from sklearn.metrics import mean_squared_error
-from sklearn.externals import joblib
 
-write_model_file = True
+
+def error_func(*args):
+    r = np.sqrt(mean_squared_error(*args))
+    print('RMSE: %9.3f' % (r))
+    return r
+
 
 project_dir = os.sep.join([os.getcwd(), '..', '..'])
 processed_data_dir = os.sep.join([project_dir, 'data', 'processed'])
@@ -43,8 +49,17 @@ for v in cat_vars:
     df[v] = df[v].astype('category').cat.as_ordered()
     # @todo Are all "ordered"? Check whether it changes the model if not.
 
+param_grid = [
+    {
+        'n_estimators': range(5, 31, 5),
+        'min_samples_split': [0.001, 0.01, 0.1],
+        'min_samples_leaf': [0.001, 0.01, 0.1],
+        'max_depth': range(3, 15, 2),
+        #'max_features': [None]
+    }]
+
 regr_model = {}
-for station in tqdm.tqdm(result_vars):
+for station in result_vars[200:201]:
     station_first_used = df_first_use.loc[
         df_first_use.station_id == int(station)]['first_use'][0]
 
@@ -58,12 +73,29 @@ for station in tqdm.tqdm(result_vars):
         df.loc[mask, station],
         indices, test_size=0.3, random_state=42)
 
-    # fit a model (one per station)
-    regr = RandomForestRegressor(
-        max_depth=7, min_samples_leaf=0.001, min_samples_split=0.001, n_estimators=30,
-        random_state=42,
-        n_jobs=-1)
-    regr.fit(X_train, y_train)
+    clf = GridSearchCV(RandomForestRegressor(random_state=42, n_jobs=-1),
+                       param_grid, cv=5,
+                       scoring=make_scorer(error_func,
+                                           greater_is_better=False))
+
+    clf.fit(X_train, y_train)
+
+print("Best parameters set found on development set:")
+print()
+print(clf.best_params_)
+print()
+print("Grid scores on development set:")
+print()
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean, std * 2, params))
+print()
+
+
+
+"""
     y_test_pred = regr.predict(X_test)
     y_train_pred = regr.predict(X_train)
     nf_0 = np.zeros((len(y_test), 1)) # net flow is zero (state persistence)
@@ -74,10 +106,10 @@ for station in tqdm.tqdm(result_vars):
     # Keep some data regarding the model for statistics; currently we also
     # save the regressor objects, while they are saved to the models dir, too.
     regr_model[station] = {
-        'rmse_train': np.sqrt(mean_squared_error(y_train, y_train_pred)),
-        'rmse_test': np.sqrt(mean_squared_error(y_test, y_test_pred)),
-        'rmse_zero_nf': np.sqrt(mean_squared_error(y_test, nf_0)),
-        'rmse_yesterday': np.sqrt(mean_squared_error(y_test, nf_y)),
+        'rmse_trn': np.sqrt(mean_squared_error(y_train, y_train_pred)),
+        'rmse_tst': np.sqrt(mean_squared_error(y_test, y_test_pred)),
+        'rmse_p': np.sqrt(mean_squared_error(y_test, nf_0)),
+        'rmse_r': np.sqrt(mean_squared_error(y_test, nf_y)),
         'lenXtrain': len(X_train),
         'first_used': station_first_used,
        }
@@ -91,7 +123,8 @@ pprint.pprint(regr_model)
 df_stats = pd.DataFrame.from_dict(data=regr_model,
                                   orient='index',
                                   columns=['first_used', 'lenXtrain',
-                                           'rmse_train', 'rmse_test',
-                                           'rmse_zero_nf', 'rmse_yesterday'])
+                                           'rmse_trn', 'rmse_tst',
+                                           'rmse_p', 'rmse_r'])
 df_stats.to_csv(os.sep.join([model_dir, 'rf50_stats.csv']))
 print(df_stats.describe())
+"""
